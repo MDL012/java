8c9bd3db211eaa000727afcce9d5c6f37046965a
MDL012 <157577467+MDL012@users.noreply.github.com>
2024-01-24 09:06:01 +0100

    valami
    valami

---
+++ Masodik.java
@@ -0,0 +1,10 @@
+public class Masodik {
+    public static void main(String[] args) {
+        System.out.print("Den");
+        String msg = "I'm happy today.";
+        int a = 150;
+        int o = 0226;
+        int h = 0x96;
+        double d = 10;
+    }
+}
