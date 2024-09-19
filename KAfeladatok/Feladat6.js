3a1e2f837e0de6603421d0a9c83739af23ace8ec
Mészáros Dénes Lajos <157577467+MDL012@users.noreply.github.com>
2024-09-19 13:30:50 +0200

    Add files via upload

--- KAfeladatok/Feladat6.js
+++ KAfeladatok/Feladat6.js
@@ -1,10 +1,20 @@
 function keres(){
+    let kereso = document.getElementById("kereso").value;//CirNOPE9
+    console.log(kereso)
     let szo0 = ""
-    fetch("https://www.codewars.com/api/v1/users/CirNOPE9")
+    fetch("https://www.codewars.com/api/v1/users/" + kereso)
     .then(x => x.json())
     .then(y => {
-        console.log(y)
-        document.getElementById("fhnev").innerHTML = "Felhasználó név: " + y.username
+        if(y.username){
+            console.log(y)
+            if(y.name == ""){
+                y.name = "---";
+            }
+            document.getElementById("kiíras").innerHTML = "\    <p>Felhasználó név: "+y.username+"</p><p>Valós név: "+ y.name +"</p><p>Összesitet pont: "+ y.honor+"</p>"; 
+        }
+        else{
+            document.getElementById("kiíras").innerHTML = "\    <h1>404 HIBA!!!</h1><h2> " + kereso +" nevű felhasználó nem létezik!</h2>"; 
+        }
     })
     
 }
